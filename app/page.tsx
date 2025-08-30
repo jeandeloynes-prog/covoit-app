7b1fa10410dae9bf44d33b6faa86298751923249
jeandeloynes-prog <jean.de.loynes@gmail.com>
2025-08-30 09:27:02 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { supabase } from "../lib/supabaseClient";
+import { supabase } from "@/lib/supabaseClient";
 import { useEffect, useState } from 'react';
 
 export default function HomePage() {
